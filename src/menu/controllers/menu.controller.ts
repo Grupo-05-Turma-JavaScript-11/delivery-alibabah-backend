7d6311c7aa01b50d25c91aac3d963fb4e044a5c9
adryanlops <adylopesk8@gmail.com>
2025-12-11 17:44:32 -0300

    add menu

---
+++ src/menu/controllers/menu.controller.ts
@@ -0,0 +1,51 @@
+import { Body, Controller, Delete, Get, HttpCode, HttpStatus, Param, ParseIntPipe, Post, Put } from "@nestjs/common";
+import { MenuService } from "../services/menu.service";
+import { Menu } from "../entities/menu.entity";
+import { DeleteResult } from "typeorm";
+
+
+@Controller("/menu")
+export class MenuController{ 
+    constructor(private readonly menuService: MenuService){}
+
+    @Get()
+    @HttpCode(HttpStatus.OK)
+    findAll(): Promise<Menu[]>{
+        return this.menuService.findAll()
+    }
+
+    @Get("/:id")
+    @HttpCode(HttpStatus.OK)
+    findById(@Param("id", ParseIntPipe) id:number): Promise<Menu>{
+       return this.menuService.findById(id)
+    }
+    
+    
+    @Get("/nome/:nome")
+    @HttpCode(HttpStatus.OK)
+    findByName(@Param("nome") nome:string): Promise<Menu[]>{
+       return this.menuService.findByName(nome)
+    }
+
+    @Post()
+    @HttpCode(HttpStatus.CREATED)
+    create(@Body() prato: Menu): Promise<Menu>{
+        return this.menuService.create(prato)
+    }
+
+
+    @Put()
+    @HttpCode(HttpStatus.OK)
+    update(@Body() prato: Menu): Promise<Menu>{
+        return this.menuService.update(prato)
+    }
+
+
+    @Delete("/:id")
+    @HttpCode(HttpStatus.NO_CONTENT)
+    delete(@Param("id", ParseIntPipe) id:number): Promise<DeleteResult>{
+        return this.menuService.delete(id)
+    }
+
+
+}
---
+++ src/menu/services/menu.service.ts
@@ -0,0 +1,56 @@
+import { ILike, Repository } from "typeorm";
+import { HttpException, HttpStatus, Injectable } from "@nestjs/common";
+import { Menu } from "../entities/menu.entity";
+import { DeleteResult } from "typeorm/browser";
+import { InjectRepository } from "@nestjs/typeorm";
+
+
+@Injectable()
+export class MenuService{
+    constructor(
+        @InjectRepository(Menu)
+        private menuRepository: Repository<Menu>
+    ){ }
+
+    async findAll(): Promise<Menu[]> {
+        return await this.menuRepository.find();
+    }
+
+    async findById(id: number): Promise<Menu> {
+        let prato = await this.menuRepository.findOne({where: {id}});
+
+        if(!prato){
+            throw new HttpException("Prato não encontrado!", HttpStatus.NOT_FOUND);
+        }
+
+        return prato;
+    }
+
+    async findByName(nome: string): Promise<Menu[]> {
+        return await this.menuRepository.find({ where: { nome: ILike(`%${nome}%`) } });
+    }
+
+    async create(prato: Menu): Promise<Menu> {
+        return await this.menuRepository.save(prato);
+    }
+
+    async update(prato: Menu): Promise<Menu> {
+        let garcom = await this.findById(prato.id);
+        
+        if(!garcom || !prato.id){
+            throw new HttpException("Prato não encontrado!", HttpStatus.NOT_FOUND);
+        }
+
+        return await this.menuRepository.save(prato);
+    }
+
+    async delete(id: number): Promise<DeleteResult> {
+        let garcom = await this.findById(id);
+        
+        if(!garcom){
+            throw new HttpException("Prato não encontrado!", HttpStatus.NOT_FOUND);
+        }
+        
+        return await this.menuRepository.delete(id);
+    }
+}
