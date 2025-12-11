2c31a26616eacfae2e2686d9a2f57ce2f3b4210e
Anderson Silva <odonto.andersonsilva@gmail.com>
2025-12-11 17:04:45 -0300

    feat(usuario): criar entity, controller e service para gerenciamento de usuários

---
+++ src/usuario/entities/usuario.entity.ts
@@ -0,0 +1,38 @@
+
+import { IsNotEmpty } from 'class-validator';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+
+
+@Entity({ name: "tb_usuario" })
+export class Usuario{
+
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @IsNotEmpty()
+    @Column({ length: 100, nullable: false })
+    nome: string;
+
+    @Column({ length: 255, nullable: true })
+    foto: string;
+
+    @IsNotEmpty()
+    @Column({ length: 255, nullable: false })
+    endereco: string;
+
+    @IsNotEmpty()
+    @Column({ length: 50, nullable: false, unique: true })
+    email: string;
+
+    @IsNotEmpty()
+    @Column({ length: 70, nullable: false })
+    senha: string;
+}
+
+   
+
+
+
+
+
