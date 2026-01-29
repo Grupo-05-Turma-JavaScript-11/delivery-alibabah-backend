b90fc549b50de1f70471c3a0ac4d7099544f7298
adryanlops <adylopesk8@gmail.com>
2026-01-29 09:29:21 -0300

    config swagger correção

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -5,7 +5,7 @@ import { ApiExcludeEndpoint } from '@nestjs/swagger';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor() {}
 
 
   @ApiExcludeEndpoint()
